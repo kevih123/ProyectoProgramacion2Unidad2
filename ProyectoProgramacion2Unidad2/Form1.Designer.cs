1a03e0ebe3866a78ce47398d15b33cfc1c985567
luisestrech <luisesparzarios7980@gmail.com>
2026-03-23 22:26:14 -0600

    Mejora de diseño

--- ProyectoProgramacion2Unidad2/Form1.Designer.cs
+++ ProyectoProgramacion2Unidad2/Form1.Designer.cs
@@ -29,16 +29,16 @@
         private void InitializeComponent()
         {
             dataGridView1 = new DataGridView();
-            numericUpDown1 = new NumericUpDown();
-            button1 = new Button();
-            button2 = new Button();
-            panel1 = new Panel();
-            label1 = new Label();
             id_alumno = new DataGridViewTextBoxColumn();
             nombre_alumn = new DataGridViewTextBoxColumn();
             apellido = new DataGridViewTextBoxColumn();
             correo = new DataGridViewTextBoxColumn();
             telefono = new DataGridViewTextBoxColumn();
+            numericUpDown1 = new NumericUpDown();
+            button1 = new Button();
+            button2 = new Button();
+            panel1 = new Panel();
+            label1 = new Label();
             label2 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
@@ -49,61 +49,15 @@
             // 
             dataGridView1.AllowUserToAddRows = false;
             dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.BackgroundColor = SystemColors.ScrollBar;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Columns.AddRange(new DataGridViewColumn[] { id_alumno, nombre_alumn, apellido, correo, telefono });
-            dataGridView1.Location = new Point(12, 92);
+            dataGridView1.Location = new Point(12, 73);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.ReadOnly = true;
-            dataGridView1.Size = new Size(509, 200);
+            dataGridView1.Size = new Size(891, 293);
             dataGridView1.TabIndex = 0;
-            // 
-            // numericUpDown1
-            // 
-            numericUpDown1.Location = new Point(48, 61);
-            numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(120, 23);
-            numericUpDown1.TabIndex = 1;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(64, 167);
-            button1.Name = "button1";
-            button1.Size = new Size(104, 33);
-            button1.TabIndex = 2;
-            button1.Text = "Cerrar";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(64, 105);
-            button2.Name = "button2";
-            button2.Size = new Size(104, 36);
-            button2.TabIndex = 3;
-            button2.Text = "Reporte";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // panel1
-            // 
-            panel1.BackColor = Color.FromArgb(11, 27, 60);
-            panel1.Controls.Add(label1);
-            panel1.Controls.Add(numericUpDown1);
-            panel1.Controls.Add(button1);
-            panel1.Controls.Add(button2);
-            panel1.Location = new Point(538, 92);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(221, 244);
-            panel1.TabIndex = 4;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.ForeColor = Color.White;
-            label1.Location = new Point(23, 21);
-            label1.Name = "label1";
-            label1.Size = new Size(180, 21);
-            label1.TabIndex = 4;
-            label1.Text = "Cantidad de alumnos: ";
             // 
             // id_alumno
             // 
@@ -135,15 +89,72 @@
             telefono.Name = "telefono";
             telefono.ReadOnly = true;
             // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            numericUpDown1.Location = new Point(331, 57);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(256, 29);
+            numericUpDown1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 122, 0);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(483, 104);
+            button1.Name = "button1";
+            button1.Size = new Size(104, 36);
+            button1.TabIndex = 2;
+            button1.Text = "Cerrar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 122, 0);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(331, 104);
+            button2.Name = "button2";
+            button2.Size = new Size(104, 36);
+            button2.TabIndex = 3;
+            button2.Text = "Reporte";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(11, 27, 60);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(numericUpDown1);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(button2);
+            panel1.Location = new Point(12, 372);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(891, 153);
+            panel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(331, 10);
+            label1.Name = "label1";
+            label1.Size = new Size(256, 32);
+            label1.TabIndex = 4;
+            label1.Text = "Cantidad de alumnos\r\n";
+            // 
             // label2
             // 
             label2.BackColor = Color.FromArgb(33, 78, 176);
             label2.Dock = DockStyle.Top;
-            label2.Font = new Font("Segoe UI Black", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             label2.ForeColor = SystemColors.ButtonFace;
             label2.Location = new Point(0, 0);
             label2.Name = "label2";
-            label2.Size = new Size(800, 45);
+            label2.Size = new Size(915, 56);
             label2.TabIndex = 5;
             label2.Text = "Proyecto";
             label2.TextAlign = ContentAlignment.MiddleCenter;
@@ -152,8 +163,8 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(67, 191, 219);
-            ClientSize = new Size(800, 450);
+            BackColor = Color.White;
+            ClientSize = new Size(915, 533);
             Controls.Add(label2);
             Controls.Add(panel1);
             Controls.Add(dataGridView1);
