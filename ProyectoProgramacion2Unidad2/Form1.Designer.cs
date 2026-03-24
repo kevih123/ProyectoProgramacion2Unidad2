c6591f16ac31525bce4ab15a00c7f553925c871e
luisestrech <luisesparzarios7980@gmail.com>
2026-03-23 23:09:23 -0600

    Diseño de formulario 2

--- ProyectoProgramacion2Unidad2/Form1.Designer.cs
+++ ProyectoProgramacion2Unidad2/Form1.Designer.cs
@@ -28,108 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            dataGridView1 = new DataGridView();
+            dvgAlumnos = new DataGridView();
             id_alumno = new DataGridViewTextBoxColumn();
             nombre_alumn = new DataGridViewTextBoxColumn();
             apellido = new DataGridViewTextBoxColumn();
             correo = new DataGridViewTextBoxColumn();
             telefono = new DataGridViewTextBoxColumn();
-            numericUpDown1 = new NumericUpDown();
-            button1 = new Button();
-            button2 = new Button();
+            nudFilas = new NumericUpDown();
+            btnCerrar = new Button();
+            btnReporte = new Button();
             panel1 = new Panel();
             label1 = new Label();
-            label2 = new Label();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            lblTitulo = new Label();
+            btnCrear = new Button();
+            ((System.ComponentModel.ISupportInitialize)dvgAlumnos).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudFilas).BeginInit();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
-            // dataGridView1
+            // dvgAlumnos
             // 
-            dataGridView1.AllowUserToAddRows = false;
-            dataGridView1.AllowUserToDeleteRows = false;
-            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridView1.BackgroundColor = SystemColors.ScrollBar;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { id_alumno, nombre_alumn, apellido, correo, telefono });
-            dataGridView1.Location = new Point(12, 73);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.ReadOnly = true;
-            dataGridView1.Size = new Size(891, 293);
-            dataGridView1.TabIndex = 0;
+            dvgAlumnos.AllowUserToAddRows = false;
+            dvgAlumnos.AllowUserToDeleteRows = false;
+            dvgAlumnos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dvgAlumnos.BackgroundColor = SystemColors.ScrollBar;
+            dvgAlumnos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dvgAlumnos.Columns.AddRange(new DataGridViewColumn[] { id_alumno, nombre_alumn, apellido, correo, telefono });
+            dvgAlumnos.Location = new Point(12, 73);
+            dvgAlumnos.Name = "dvgAlumnos";
+            dvgAlumnos.Size = new Size(891, 293);
+            dvgAlumnos.TabIndex = 0;
             // 
             // id_alumno
             // 
             id_alumno.HeaderText = "ID";
             id_alumno.Name = "id_alumno";
-            id_alumno.ReadOnly = true;
             // 
             // nombre_alumn
             // 
             nombre_alumn.HeaderText = "Nombre";
             nombre_alumn.Name = "nombre_alumn";
-            nombre_alumn.ReadOnly = true;
             // 
             // apellido
             // 
             apellido.HeaderText = "Apellido";
             apellido.Name = "apellido";
-            apellido.ReadOnly = true;
             // 
             // correo
             // 
             correo.HeaderText = "Correo";
             correo.Name = "correo";
-            correo.ReadOnly = true;
             // 
             // telefono
             // 
             telefono.HeaderText = "Teléfono";
             telefono.Name = "telefono";
-            telefono.ReadOnly = true;
             // 
-            // numericUpDown1
+            // nudFilas
             // 
-            numericUpDown1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            numericUpDown1.Location = new Point(331, 57);
-            numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(256, 29);
-            numericUpDown1.TabIndex = 1;
+            nudFilas.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            nudFilas.Location = new Point(281, 57);
+            nudFilas.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
+            nudFilas.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            nudFilas.Name = "nudFilas";
+            nudFilas.Size = new Size(339, 29);
+            nudFilas.TabIndex = 1;
+            nudFilas.Tag = "";
+            nudFilas.Value = new decimal(new int[] { 1, 0, 0, 0 });
             // 
-            // button1
+            // btnCerrar
             // 
-            button1.BackColor = Color.FromArgb(255, 122, 0);
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.ForeColor = Color.White;
-            button1.Location = new Point(483, 104);
-            button1.Name = "button1";
-            button1.Size = new Size(104, 36);
-            button1.TabIndex = 2;
-            button1.Text = "Cerrar";
-            button1.UseVisualStyleBackColor = false;
+            btnCerrar.BackColor = Color.FromArgb(255, 122, 0);
+            btnCerrar.FlatStyle = FlatStyle.Flat;
+            btnCerrar.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCerrar.ForeColor = Color.White;
+            btnCerrar.Location = new Point(534, 104);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(109, 36);
+            btnCerrar.TabIndex = 2;
+            btnCerrar.Text = "Cerrar";
+            btnCerrar.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // btnReporte
             // 
-            button2.BackColor = Color.FromArgb(255, 122, 0);
-            button2.FlatStyle = FlatStyle.Flat;
-            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button2.ForeColor = Color.White;
-            button2.Location = new Point(331, 104);
-            button2.Name = "button2";
-            button2.Size = new Size(104, 36);
-            button2.TabIndex = 3;
-            button2.Text = "Reporte";
-            button2.UseVisualStyleBackColor = false;
+            btnReporte.BackColor = Color.FromArgb(255, 122, 0);
+            btnReporte.FlatStyle = FlatStyle.Flat;
+            btnReporte.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnReporte.ForeColor = Color.White;
+            btnReporte.Location = new Point(392, 104);
+            btnReporte.Name = "btnReporte";
+            btnReporte.Size = new Size(109, 36);
+            btnReporte.TabIndex = 3;
+            btnReporte.Text = "Ver Reporte";
+            btnReporte.UseVisualStyleBackColor = false;
             // 
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(11, 27, 60);
+            panel1.Controls.Add(btnCrear);
             panel1.Controls.Add(label1);
-            panel1.Controls.Add(numericUpDown1);
-            panel1.Controls.Add(button1);
-            panel1.Controls.Add(button2);
+            panel1.Controls.Add(nudFilas);
+            panel1.Controls.Add(btnCerrar);
+            panel1.Controls.Add(btnReporte);
             panel1.Location = new Point(12, 372);
             panel1.Name = "panel1";
             panel1.Size = new Size(891, 153);
@@ -146,18 +146,31 @@
             label1.TabIndex = 4;
             label1.Text = "Cantidad de alumnos\r\n";
             // 
-            // label2
+            // lblTitulo
             // 
-            label2.BackColor = Color.FromArgb(33, 78, 176);
-            label2.Dock = DockStyle.Top;
-            label2.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            label2.ForeColor = SystemColors.ButtonFace;
-            label2.Location = new Point(0, 0);
-            label2.Name = "label2";
-            label2.Size = new Size(915, 56);
-            label2.TabIndex = 5;
-            label2.Text = "Proyecto";
-            label2.TextAlign = ContentAlignment.MiddleCenter;
+            lblTitulo.BackColor = Color.FromArgb(33, 78, 176);
+            lblTitulo.Dock = DockStyle.Top;
+            lblTitulo.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblTitulo.ForeColor = SystemColors.ButtonFace;
+            lblTitulo.Location = new Point(0, 0);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(915, 56);
+            lblTitulo.TabIndex = 5;
+            lblTitulo.Text = "Práctica 3 -Registro de Alumnos";
+            lblTitulo.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // btnCrear
+            // 
+            btnCrear.BackColor = Color.FromArgb(255, 122, 0);
+            btnCrear.FlatStyle = FlatStyle.Flat;
+            btnCrear.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCrear.ForeColor = Color.White;
+            btnCrear.Location = new Point(252, 104);
+            btnCrear.Name = "btnCrear";
+            btnCrear.Size = new Size(109, 36);
+            btnCrear.TabIndex = 5;
+            btnCrear.Text = "Crear Tabla";
+            btnCrear.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -165,13 +178,13 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(915, 533);
-            Controls.Add(label2);
+            Controls.Add(lblTitulo);
             Controls.Add(panel1);
-            Controls.Add(dataGridView1);
+            Controls.Add(dvgAlumnos);
             Name = "Form1";
             Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dvgAlumnos).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudFilas).EndInit();
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ResumeLayout(false);
@@ -179,10 +192,10 @@
 
         #endregion
 
-        private DataGridView dataGridView1;
-        private NumericUpDown numericUpDown1;
-        private Button button1;
-        private Button button2;
+        private DataGridView dvgAlumnos;
+        private NumericUpDown nudFilas;
+        private Button btnCerrar;
+        private Button btnReporte;
         private Panel panel1;
         private DataGridViewTextBoxColumn id_alumno;
         private DataGridViewTextBoxColumn nombre_alumn;
@@ -190,6 +203,7 @@
         private DataGridViewTextBoxColumn correo;
         private DataGridViewTextBoxColumn telefono;
         private Label label1;
-        private Label label2;
+        private Label lblTitulo;
+        private Button btnCrear;
     }
 }
---
+++ ProyectoProgramacion2Unidad2/Form2.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace ProyectoProgramacion2Unidad2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblTitulo = new Label();
+            panel1 = new Panel();
+            lblSelID = new Label();
+            cmbID = new ComboBox();
+            btnGenerar = new Button();
+            picFoto = new PictureBox();
+            lblFecha = new Label();
+            btnRegresar = new Button();
+            btnCerrar = new Button();
+            lblID = new Label();
+            lblNombre = new Label();
+            lblApellido = new Label();
+            lblCorreo = new Label();
+            lblTelefono = new Label();
+            lblCarrera = new Label();
+            lblSemestre = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)picFoto).BeginInit();
+            SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.BackColor = Color.FromArgb(33, 78, 176);
+            lblTitulo.Dock = DockStyle.Top;
+            lblTitulo.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblTitulo.ForeColor = SystemColors.ButtonFace;
+            lblTitulo.Location = new Point(0, 0);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(915, 65);
+            lblTitulo.TabIndex = 6;
+            lblTitulo.Text = "Reporte de Alumno";
+            lblTitulo.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ScrollBar;
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.Controls.Add(lblSemestre);
+            panel1.Controls.Add(lblCarrera);
+            panel1.Controls.Add(lblTelefono);
+            panel1.Controls.Add(lblCorreo);
+            panel1.Controls.Add(lblApellido);
+            panel1.Controls.Add(lblNombre);
+            panel1.Controls.Add(lblID);
+            panel1.Controls.Add(btnCerrar);
+            panel1.Controls.Add(btnRegresar);
+            panel1.Controls.Add(lblFecha);
+            panel1.Controls.Add(btnGenerar);
+            panel1.Controls.Add(cmbID);
+            panel1.Controls.Add(lblSelID);
+            panel1.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            panel1.Location = new Point(83, 80);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(747, 441);
+            panel1.TabIndex = 7;
+            // 
+            // lblSelID
+            // 
+            lblSelID.AutoSize = true;
+            lblSelID.Location = new Point(14, 9);
+            lblSelID.Name = "lblSelID";
+            lblSelID.Size = new Size(154, 25);
+            lblSelID.TabIndex = 0;
+            lblSelID.Text = "Selecciona un ID";
+            // 
+            // cmbID
+            // 
+            cmbID.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbID.FlatStyle = FlatStyle.Popup;
+            cmbID.FormattingEnabled = true;
+            cmbID.Location = new Point(14, 37);
+            cmbID.Name = "cmbID";
+            cmbID.Size = new Size(133, 33);
+            cmbID.TabIndex = 1;
+            // 
+            // btnGenerar
+            // 
+            btnGenerar.BackColor = Color.FromArgb(11, 27, 60);
+            btnGenerar.FlatStyle = FlatStyle.Popup;
+            btnGenerar.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnGenerar.ForeColor = Color.White;
+            btnGenerar.Location = new Point(107, 388);
+            btnGenerar.Name = "btnGenerar";
+            btnGenerar.Size = new Size(152, 44);
+            btnGenerar.TabIndex = 4;
+            btnGenerar.Text = "Generar Reporte";
+            btnGenerar.UseVisualStyleBackColor = false;
+            // 
+            // picFoto
+            // 
+            picFoto.Location = new Point(833, 1);
+            picFoto.Name = "picFoto";
+            picFoto.Size = new Size(80, 80);
+            picFoto.SizeMode = PictureBoxSizeMode.Zoom;
+            picFoto.TabIndex = 5;
+            picFoto.TabStop = false;
+            // 
+            // lblFecha
+            // 
+            lblFecha.AutoSize = true;
+            lblFecha.Location = new Point(577, 9);
+            lblFecha.Name = "lblFecha";
+            lblFecha.Size = new Size(62, 25);
+            lblFecha.TabIndex = 5;
+            lblFecha.Text = "Fecha";
+            // 
+            // btnRegresar
+            // 
+            btnRegresar.BackColor = Color.FromArgb(11, 27, 60);
+            btnRegresar.FlatStyle = FlatStyle.Popup;
+            btnRegresar.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnRegresar.ForeColor = Color.White;
+            btnRegresar.Location = new Point(299, 388);
+            btnRegresar.Name = "btnRegresar";
+            btnRegresar.Size = new Size(152, 44);
+            btnRegresar.TabIndex = 6;
+            btnRegresar.Text = "Regresar";
+            btnRegresar.UseVisualStyleBackColor = false;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.BackColor = Color.FromArgb(11, 27, 60);
+            btnCerrar.FlatStyle = FlatStyle.Popup;
+            btnCerrar.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCerrar.ForeColor = Color.White;
+            btnCerrar.Location = new Point(487, 388);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(152, 44);
+            btnCerrar.TabIndex = 7;
+            btnCerrar.Text = "Cerrar";
+            btnCerrar.UseVisualStyleBackColor = false;
+            // 
+            // lblID
+            // 
+            lblID.BackColor = SystemColors.Menu;
+            lblID.Location = new Point(107, 78);
+            lblID.Name = "lblID";
+            lblID.Size = new Size(532, 30);
+            lblID.TabIndex = 8;
+            lblID.Text = "ID";
+            // 
+            // lblNombre
+            // 
+            lblNombre.BackColor = SystemColors.Menu;
+            lblNombre.Location = new Point(107, 119);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(532, 30);
+            lblNombre.TabIndex = 9;
+            lblNombre.Text = "Nombre";
+            // 
+            // lblApellido
+            // 
+            lblApellido.BackColor = SystemColors.Menu;
+            lblApellido.Location = new Point(107, 163);
+            lblApellido.Name = "lblApellido";
+            lblApellido.Size = new Size(532, 30);
+            lblApellido.TabIndex = 10;
+            lblApellido.Text = "Apellido";
+            // 
+            // lblCorreo
+            // 
+            lblCorreo.BackColor = SystemColors.Menu;
+            lblCorreo.Location = new Point(107, 206);
+            lblCorreo.Name = "lblCorreo";
+            lblCorreo.Size = new Size(532, 30);
+            lblCorreo.TabIndex = 11;
+            lblCorreo.Text = "Correo";
+            // 
+            // lblTelefono
+            // 
+            lblTelefono.BackColor = SystemColors.Menu;
+            lblTelefono.Location = new Point(105, 249);
+            lblTelefono.Name = "lblTelefono";
+            lblTelefono.Size = new Size(532, 30);
+            lblTelefono.TabIndex = 12;
+            lblTelefono.Text = "Telefono";
+            // 
+            // lblCarrera
+            // 
+            lblCarrera.BackColor = SystemColors.Menu;
+            lblCarrera.Location = new Point(105, 292);
+            lblCarrera.Name = "lblCarrera";
+            lblCarrera.Size = new Size(532, 30);
+            lblCarrera.TabIndex = 13;
+            lblCarrera.Text = "Carrera";
+            // 
+            // lblSemestre
+            // 
+            lblSemestre.BackColor = SystemColors.Menu;
+            lblSemestre.Location = new Point(105, 335);
+            lblSemestre.Name = "lblSemestre";
+            lblSemestre.Size = new Size(532, 30);
+            lblSemestre.TabIndex = 14;
+            lblSemestre.Text = "Semestre";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(915, 533);
+            Controls.Add(picFoto);
+            Controls.Add(panel1);
+            Controls.Add(lblTitulo);
+            Name = "Form2";
+            Text = "Form2";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)picFoto).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label lblTitulo;
+        private Panel panel1;
+        private ComboBox cmbID;
+        private Label lblSelID;
+        private Button btnGenerar;
+        private PictureBox picFoto;
+        private Label lblFecha;
+        private Label lblID;
+        private Button btnCerrar;
+        private Button btnRegresar;
+        private Label lblSemestre;
+        private Label lblCarrera;
+        private Label lblTelefono;
+        private Label lblCorreo;
+        private Label lblApellido;
+        private Label lblNombre;
+    }
+}
