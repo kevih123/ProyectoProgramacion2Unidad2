88c84fd632d8caa937f850ebd0b4106ec14582a8
Miguel <palaciogarciamiguel37@gmail.com>
2026-03-23 23:32:13 -0600

    Pequeño rediseño al Form1 de Luis

--- ProyectoProgramacion2Unidad2/Form1.Designer.cs
+++ ProyectoProgramacion2Unidad2/Form1.Designer.cs
@@ -37,9 +37,9 @@
             numericUpDown1 = new NumericUpDown();
             button1 = new Button();
             button2 = new Button();
-            panel1 = new Panel();
             label1 = new Label();
             label2 = new Label();
+            panel1 = new Panel();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             panel1.SuspendLayout();
@@ -53,96 +53,94 @@
             dataGridView1.BackgroundColor = SystemColors.ScrollBar;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Columns.AddRange(new DataGridViewColumn[] { id_alumno, nombre_alumn, apellido, correo, telefono });
-            dataGridView1.Location = new Point(12, 73);
+            dataGridView1.Location = new Point(14, 97);
+            dataGridView1.Margin = new Padding(3, 4, 3, 4);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.ReadOnly = true;
-            dataGridView1.Size = new Size(891, 293);
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(899, 391);
             dataGridView1.TabIndex = 0;
             // 
             // id_alumno
             // 
             id_alumno.HeaderText = "ID";
+            id_alumno.MinimumWidth = 6;
             id_alumno.Name = "id_alumno";
             id_alumno.ReadOnly = true;
             // 
             // nombre_alumn
             // 
             nombre_alumn.HeaderText = "Nombre";
+            nombre_alumn.MinimumWidth = 6;
             nombre_alumn.Name = "nombre_alumn";
             nombre_alumn.ReadOnly = true;
             // 
             // apellido
             // 
             apellido.HeaderText = "Apellido";
+            apellido.MinimumWidth = 6;
             apellido.Name = "apellido";
             apellido.ReadOnly = true;
             // 
             // correo
             // 
             correo.HeaderText = "Correo";
+            correo.MinimumWidth = 6;
             correo.Name = "correo";
             correo.ReadOnly = true;
             // 
             // telefono
             // 
             telefono.HeaderText = "Teléfono";
+            telefono.MinimumWidth = 6;
             telefono.Name = "telefono";
             telefono.ReadOnly = true;
             // 
             // numericUpDown1
             // 
             numericUpDown1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            numericUpDown1.Location = new Point(331, 57);
+            numericUpDown1.Location = new Point(33, 79);
+            numericUpDown1.Margin = new Padding(3, 4, 3, 4);
             numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(256, 29);
+            numericUpDown1.Size = new Size(293, 34);
             numericUpDown1.TabIndex = 1;
             // 
             // button1
             // 
-            button1.BackColor = Color.FromArgb(255, 122, 0);
+            button1.BackColor = Color.Crimson;
             button1.FlatStyle = FlatStyle.Flat;
             button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button1.ForeColor = Color.White;
-            button1.Location = new Point(483, 104);
+            button1.Location = new Point(123, 234);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(104, 36);
+            button1.Size = new Size(119, 48);
             button1.TabIndex = 2;
             button1.Text = "Cerrar";
             button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
-            button2.BackColor = Color.FromArgb(255, 122, 0);
+            button2.BackColor = Color.Azure;
             button2.FlatStyle = FlatStyle.Flat;
             button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button2.ForeColor = Color.White;
-            button2.Location = new Point(331, 104);
+            button2.ForeColor = Color.Black;
+            button2.Location = new Point(123, 151);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(104, 36);
+            button2.Size = new Size(119, 48);
             button2.TabIndex = 3;
             button2.Text = "Reporte";
             button2.UseVisualStyleBackColor = false;
-            // 
-            // panel1
-            // 
-            panel1.BackColor = Color.FromArgb(11, 27, 60);
-            panel1.Controls.Add(label1);
-            panel1.Controls.Add(numericUpDown1);
-            panel1.Controls.Add(button1);
-            panel1.Controls.Add(button2);
-            panel1.Location = new Point(12, 372);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(891, 153);
-            panel1.TabIndex = 4;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.White;
-            label1.Location = new Point(331, 10);
+            label1.Location = new Point(23, 20);
             label1.Name = "label1";
-            label1.Size = new Size(256, 32);
+            label1.Size = new Size(316, 41);
             label1.TabIndex = 4;
             label1.Text = "Cantidad de alumnos\r\n";
             // 
@@ -154,20 +152,33 @@
             label2.ForeColor = SystemColors.ButtonFace;
             label2.Location = new Point(0, 0);
             label2.Name = "label2";
-            label2.Size = new Size(915, 56);
+            label2.Size = new Size(1320, 75);
             label2.TabIndex = 5;
-            label2.Text = "Proyecto";
+            label2.Text = "Programación II - Proyecto Unidad 2";
             label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(11, 27, 60);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(numericUpDown1);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(932, 97);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(361, 391);
+            panel1.TabIndex = 6;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(915, 533);
-            Controls.Add(label2);
+            ClientSize = new Size(1320, 504);
             Controls.Add(panel1);
+            Controls.Add(label2);
             Controls.Add(dataGridView1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
@@ -183,7 +194,6 @@
         private NumericUpDown numericUpDown1;
         private Button button1;
         private Button button2;
-        private Panel panel1;
         private DataGridViewTextBoxColumn id_alumno;
         private DataGridViewTextBoxColumn nombre_alumn;
         private DataGridViewTextBoxColumn apellido;
@@ -191,5 +201,6 @@
         private DataGridViewTextBoxColumn telefono;
         private Label label1;
         private Label label2;
+        private Panel panel1;
     }
 }
